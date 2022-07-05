5dc418678a1ea7361479d7c7a31e4d829d5d23af
bergerson92 <90724387+bergerson92@users.noreply.github.com>
2022-07-05 16:19:12 +0200

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,33 @@
+let x;
+let y;
+let radius = 25;
+
+let xSpeed;
+let ySpeed;
+
+function setup() {
+  createCanvas(400, 400);
+  x = width  /2;
+  y = height /2;
+  
+  xSpeed = random(2, 5);
+  ySpeed = random(2, 5);
+  }
+
+function draw() {
+  background(220);
+  fill(100, 123, 255);
+  ellipse(x, y, radius * 2, radius * 2);
+  
+  x += xSpeed;
+  y += ySpeed;
+  
+  if (x > width - radius || x < radius) {
+    xSpeed = -xSpeed;
+  }
+  
+  if(y > height - radius || y < radius) {
+    ySpeed = -ySpeed;
+  }
+  
+}
